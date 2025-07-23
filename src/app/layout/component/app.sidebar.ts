df4787d7e8e31a08a190ccffce46db12ad1b923f
guilhermeroncon <guilhermeroncs@gmail.com>
2025-07-23 14:15:13 -0300

    mudei
    coisas

--- src/app/layout/component/app.sidebar.ts
+++ src/app/layout/component/app.sidebar.ts
@@ -5,9 +5,16 @@ import { AppMenu } from './app.menu';
     selector: 'app-sidebar',
     standalone: true,
     imports: [AppMenu],
-    template: ` <div class="layout-sidebar">
-        <app-menu></app-menu>
-    </div>`
+    template: ` 
+        <div class="layout-sidebar">
+            <div class="layout-sidebar-profile">
+                <img src="assets/layout/images/avatar.png" alt="User Avatar" class="layout-sidebar-profile-avatar" />
+                <h4 class="layout-sidebar-profile-name">Guilherme</h4>
+                <span class="layout-sidebar-profile-role">Administrador</span>
+            </div>
+            <app-menu></app-menu>
+        </div>
+    `
 })
 export class AppSidebar {
     constructor(public el: ElementRef) {}
